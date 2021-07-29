50f4854d0671086c9fcd16dd68c18f6cc49b0019
jiangxl <jiangxl@hicore.com>
2021-07-29 14:00:04 +0800

    提交代码

--- MicroService/MicroService.AggregateService/Startup.cs
+++ MicroService/MicroService.AggregateService/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Polly;
 using System;
+using System.Net;
 using System.Net.Http;
 
 namespace MicroService.AggregateService
